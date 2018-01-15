98accdbf27846053fed92c828772241de4aba8b8
Soongja <david0801@naver.com>
2018-01-16 00:28:17 +0900

    revised font2img

--- font2img.py
+++ font2img.py
@@ -33,13 +33,15 @@ def draw_example(ch, src_font, dst_font, canvas_size, x_offset, y_offset):
 
 def font2img(src, dst, charset,
              char_size, canvas_size, x_offset, y_offset,
-             sample_dir, label):
+             sample_count, sample_dir, label):
     src_font = ImageFont.truetype(src, size=char_size)
     dst_font = ImageFont.truetype(dst, size=char_size)
 
     count = 0
 
     for c in charset:
+        if count == sample_count:
+            break
         e = draw_example(c, src_font, dst_font, canvas_size, x_offset, y_offset)
         if e:
             e.save(os.path.join(sample_dir, "%d_%04d.jpg" % (label, count)))
@@ -58,13 +60,16 @@ parser.add_argument('--y_offset', dest='y_offset', type=int, default=20, help='y
 parser.add_argument('--sample_dir', dest='sample_dir', help='directory to save examples')
 parser.add_argument('--label', dest='label', type=int, default=0, help='label as the prefix of examples')
 parser.add_argument('--unicode', dest='unicode', help='path of the unicode list')
+parser.add_argument('--sample_count', dest='sample_count', type=int, default=12000, help='number of characters to draw')
 
 args = parser.parse_args()
 
 if __name__ == "__main__":
     charset = open(args.unicode).read().split('\n')
+    #random.shuffle(a)
+    #charset = a[:200]
     if args.shuffle:
         random.shuffle(charset)
     font2img(args.src_font, args.dst_font, charset,
              args.char_size, args.canvas_size, args.x_offset, args.y_offset,
-             args.sample_dir, args.label)
+             args.sample_count, args.sample_dir, args.label)
